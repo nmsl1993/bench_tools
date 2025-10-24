c933685993dca9c6e5483392d843c699529124ce
Noah Levy <noahmslevy@gmail.com>
2025-10-24 13:02:29 -0700

    copy uboot dtb in flash_uboot

--- flash_uboot.sh
+++ flash_uboot.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
+TFTP_DIR=/srv/tftp
+IMAGES_DIR=/home/noah/Development/buildroot/output/images
+
+rsync -av --progress ${IMAGES_DIR}/u-boot-dtb.imx ${TFTP_DIR}/u-boot-dtb.imx
+rsync -av --progress ${IMAGES_DIR}/imx6ull-ninefives-poe.dtb ${TFTP_DIR}/imx6ull-ninefives-poe.dtb
+rsync -av --progress ${IMAGES_DIR}/zImage ${TFTP_DIR}/zImage
+
+
 BUILDROOT_DIR=/home/noah/Development/buildroot
 python3 /home/noah/Development/bench_tools/ch1_off.py
 sleep 0.5
 python3 /home/noah/Development/bench_tools/ch1_on.py
 ls -ltrah ${BUILDROOT_DIR}/output/images/*.imx
-uuu -v ${BUILDROOT_DIR}/output/images/u-boot-dtb.imx
+uuu -v ${TFTP_DIR}/u-boot-dtb.imx
+
 
